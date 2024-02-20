4ce44739a45153078dfbe9e0117df10130bed982
Stefan Lahtinen <stefan.lahtinen@gmail.com>
2024-02-21 00:10:46 +0100

    Register user

---
+++ scripts/login.js
@@ -0,0 +1,45 @@
+const registerNewUserButton = document.querySelector("#register-button");
+const addName = document.querySelector("#namn");
+const addEmail = document.querySelector("#email");
+const addMobilNummer = document.querySelector("#mobilnummer");
+const addFaktureringsadress = document.querySelector("#faktureringsadress");
+
+
+const registerNewUser = async(user) => {
+    const url = "http://localhost:3000/users";
+
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(user),
+        });
+    
+        if(response.ok){
+            return await response.json();
+            
+        }else{
+            console.log("Error: users not found");
+        }
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+
+const registerNewUserHandler = async(e) => {
+    e.preventDefault();
+
+    const user = {
+        kundnamn: addName.value,
+        epostadress: addEmail.value,
+        mobilnummer: addMobilNummer.value,
+        faktureringsadress: addFaktureringsadress.value
+    };
+
+    const registeredNewUser = await registerNewUser(user);
+}
+
+registerNewUserButton.addEventListener('click', registerNewUserHandler);
